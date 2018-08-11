42dcdcc8bf5c94c360f440f32ab63184a1fddb72
Murat Ödünç <murat.asya@gmail.com>
2018-08-11 16:38:27 +0300

    "System.IO.Ports" doesnt support MacOS

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO.Ports;
+namespace ReadingDataOnESP8266
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Listing ports... \n");
+            foreach (var portName in SerialPort.GetPortNames())
+            {
+                Console.WriteLine(System.String.Format("Port names: {0}", portName));
+            }
+        }
+    }
+}
